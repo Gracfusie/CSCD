0ae050db37d980b07d170de22f1545befcc38893
mrwu2004 <1195296820@qq.com>
2025-10-30 21:09:16 +0800

    2025-10-30-21:09

--- PE/src/NPU_top.sv
+++ PE/src/NPU_top.sv
@@ -5,7 +5,11 @@
 // Map this block to a base address (e.g., 0x7000_0000) via the AXI crossbar,
 // and bridge with axi2mem like SRAM (see lab example).  :contentReference[oaicite:3]{index=3}
 // -----------------------------------------------------------------------------
-module pe_top #(
+module NPU_top #(
+  parameter int N = 10,
+  parameter int K_SIZE = 3,
+  parameter int DATA_WIDTH = 8,
+  parameter int AXI_WIDTH = 32,
   parameter int ADDR_W = 3  // enough for 0..4
 ) (
   input  logic               clk,
@@ -15,10 +19,28 @@ module pe_top #(
   input  logic               req_i,        // access qualifier (assume always 1'b1 OK)
   input  logic [3:0]         wen_i,        // byte enables; write if any bit=1
   input  logic [ADDR_W-1:0]  addr_i,       // word index (offset / 4)
-  input  logic [31:0]        wdata_i,
-  output logic [31:0]        rdata_o
+  input  logic [AXI_WIDTH-1:0]        wdata_i,
+  output logic [AXI_WIDTH-1:0]        rdata_o
 );
 
+  // NPU buffer
+  parameter int BUFFER_DEPTH = (N+1)*K_SIZE;
+  logic [DATA_WIDTH-1:0] npu_buffer [BUFFER_DEPTH-1:0];
+
+  // Write demux
+  pe_demux #(
+    .DATA_WIDTH (K_SIZE*DATA_WIDTH),
+    .DATA_DEPTH (BUFFER_DEPTH),
+    .SEL_WIDTH  ($clog2(BUFFER_DEPTH))
+  ) u_demux (
+    .data_in (wdata_i[K_SIZE*DATA_WIDTH-1:0]),
+    .sel     (addr_i[ADDR_W-1:0]),
+    .en      (|wen_i),
+    .data_out(npu_buffer)
+  );
+
   
+
+
 
 endmodule
--- PE/src/pe_dmux.sv
+++ PE/src/pe_dmux.sv
@@ -7,12 +7,13 @@
 
 module demux #(
     parameter DATA_WIDTH = 8,
-    parameter SEL_WIDTH  = 2
+    parameter DATA_DEPTH = 33,
+    parameter SEL_WIDTH  = $clog2(DATA_DEPTH)
 ) (
     input  logic [DATA_WIDTH-1:0] data_in,
     input  logic [SEL_WIDTH-1:0]  sel,
     input  logic                  en,
-    output logic [DATA_WIDTH-1:0] data_out [0:(1<<SEL_WIDTH)-1]
+    output logic [DATA_WIDTH-1:0] data_out [DATA_DEPTH-1:0]
 );
 
     localparam NUM_OUTPUTS = 1 << SEL_WIDTH;
